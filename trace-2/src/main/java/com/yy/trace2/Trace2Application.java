aa850c48c2cb1fedccd91b845bd3ca9bf41de3b3
yuanyuan.jing <aa>
2018-12-05 15:22:09 +0800

    使用spring-cloud-starter-sleuth组件实现服务跟踪

---
+++ trace-2/src/main/java/com/yy/trace2/Trace2Application.java
@@ -0,0 +1,26 @@
+package com.yy.trace2;
+
+import org.apache.log4j.Logger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableDiscoveryClient
+@SpringBootApplication
+public class Trace2Application {
+    private final Logger logger = Logger.getLogger(getClass());
+
+    @RequestMapping(value = "/trace-2", method = RequestMethod.GET)
+    public String trace() {
+        logger.info("===<call trace-2>===");
+        return "Trace";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Trace2Application.class, args);
+    }
+}
